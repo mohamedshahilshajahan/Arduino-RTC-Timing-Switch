661b023e0e5321959babebeee202e0ca7e1b8257
Mohamed Shahil <152691954+mohamedshahilshajahan@users.noreply.github.com>
2024-02-25 19:32:07 +0530

    Add files via upload

---
+++ DS1307_3_0.ino
@@ -0,0 +1,53 @@
+#include <Wire.h>
+#include <RTClib.h>
+
+RTC_DS1307 rtc;
+
+const int relay = A3;
+bool on = LOW;
+bool off = HIGH;
+
+void setup() {
+  Serial.begin(9600);
+
+  if (!rtc.begin()) {
+    Serial.println("Couldn't find RTC. Check your connections!");
+    while (1);
+  }
+
+  if (!rtc.isrunning()) {
+    Serial.println("RTC is not running, setting the time!");
+    rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  }
+
+  pinMode(relay, OUTPUT);
+  digitalWrite(relay, off);
+}
+
+void loop() {
+  DateTime now = rtc.now();
+  int currentHour = now.hour();
+  int currentMinute = now.minute();
+
+  Serial.print("Current time: ");
+  Serial.print(now.year(), DEC);
+  Serial.print('/');
+  Serial.print(now.month(), DEC);
+  Serial.print('/');
+  Serial.print(now.day(), DEC);
+  Serial.print(" ");
+  Serial.print(now.hour(), DEC);
+  Serial.print(':');
+  Serial.print(now.minute(), DEC);
+  Serial.print(':');
+  Serial.println(now.second(), DEC);
+
+  //Specify the desired timing for your preference.             #Configure the activation time for the relay.
+  if ((currentHour == 17 && currentMinute >= 40) || (currentHour >= 18 && currentHour < 24) || (currentHour >= 0 && currentHour < 6)) {
+    digitalWrite(relay, on);
+  } else {
+    digitalWrite(relay, off);
+  }
+
+  delay(1000);
+}
